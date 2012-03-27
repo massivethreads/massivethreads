f7c859c9e4b9d5d85029039cdf1dab59f3ae6704
Taura.Kenjiro <Taura.Kenjiro@gmail.com>
2012-03-27 11:01:09 +0000

    make install now installs header files

---
+++ mttb/task_group.h
@@ -0,0 +1,63 @@
+#pragma once
+#include <assert.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <functional>
+
+struct task {
+  pthread_t tid;
+  std::function<void ()> f;
+};
+
+void * invoke_task(void * arg_) {
+  task * arg = (task *)arg_;
+  std::function<void()> f = arg->f;
+  f();
+  return arg_;
+}
+
+#if !defined(INIT_SZ)
+#define TASK_GROUP_INIT_SZ 10
+#endif
+
+struct task_group {
+  int n;
+  int capacity;
+  task tasks_[TASK_GROUP_INIT_SZ];
+  task * tasks;
+  task_group() {
+    n = 0;
+    capacity = TASK_GROUP_INIT_SZ;
+    tasks = tasks_;
+  }
+  void extend() {
+    int new_capacity = capacity + capacity + 1;
+    task * new_tasks = (task *)malloc(sizeof(task) * new_capacity);
+    assert(new_tasks);
+    memcpy(new_tasks, tasks, sizeof(task) * capacity);
+    if (tasks == tasks_) {
+      memset(tasks, 0, sizeof(task) * capacity);
+    } else {
+      free(tasks);
+    }
+    tasks = new_tasks;
+    capacity = new_capacity;
+  }
+  void run(std::function<void ()> f) {
+    if (n == capacity) extend();
+    task * t = &tasks[n];
+    t->f = f;
+    n++;
+    pthread_create(&t->tid, NULL, invoke_task, (void*)t);
+  }
+  void wait() {
+    int i;
+    for (i = 0; i < n; i++) {
+      void * ret;
+      pthread_join(tasks[i].tid, &ret);
+    }
+    n = 0;
+  }
+};
