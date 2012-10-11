b9e886617bdf81e0d106a74e51d9e6146d0feae7
Nan.Dun <nan.dun@gmail.com>
2012-10-11 15:34:37 +0000

    sparcv8 context switch

--- examples/bench/simple.c
+++ examples/bench/simple.c
@@ -5,7 +5,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <pthread.h>
+//#include <pthread.h>
+#include "../../myth.h"
 
 void * func1(void *arg)
 {
@@ -19,17 +20,22 @@ void * func2(void *arg)
 
 int main(int argc, char *argv)
 {
-  printf("start\n");
 	/* MYTH_NATIVE test 
+  myth_thread_t th1;
+  printf("init\n");
   myth_init();
-  printf("run\n");
+  printf("create\n");
+  th1 = myth_create(func1, 0x0);
+  printf("join\n");
+  myth_join(th1, NULL);
+  printf("fini\n");
 	myth_fini();
+  printf("exit\n");
    */
   pthread_t th1, th2;
   pthread_create(&th1, NULL, func1, NULL);
   pthread_create(&th2, NULL, func2, NULL);
   pthread_join(th1, NULL);
   pthread_join(th2, NULL);
-  printf("start\n");
   return 0;
 }
