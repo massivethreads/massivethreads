fa3317cd3225e5af8cde9745ffa161c20e4dc779
nakashima@logos.ic.i.u-tokyo.ac.jp <nakashima@logos.ic.i.u-tokyo.ac.jp@dd21a00a-c728-a333-246e-3318298a4437>
2012-10-16 06:43:03 +0000

    Backport improvements from MassiveThreads on Chapel source tree: Worker thread binding can be enabled/disabled using environemnt variable MYTH_BIND_WORKERS

--- trunk/chapel-if/1.5/runtime/include/tasks/massivethreads/tasks-massivethreads.h
+++ trunk/chapel-if/1.5/runtime/include/tasks/massivethreads/tasks-massivethreads.h
@@ -1,17 +1,13 @@
-/**************************************************************************
-  Copyright (c) 2004-2011, Cray Inc.  (See LICENSE file for more details)
-**************************************************************************/
-
-#ifndef _tasks_massivethreads_h_
-#define _tasks_massivethreads_h_
+#ifndef _tasks_myth_h_
+#define _tasks_myth_h_
 
 #define CHPL_COMM_YIELD_TASK_WHILE_POLLING
 
 #include "myth.h"
 
 typedef struct{
-	myth_felock_t lock;
-	volatile int is_full;
+        myth_felock_t lock;
+        volatile int is_full;
 }chpl_sync_aux_t;
 
 // type (and default value) used to communicate task identifiers
