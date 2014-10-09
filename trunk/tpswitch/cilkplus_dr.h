27850b73bd9c0fc1a842d5cec4fcde80ae7e4374
taura.kenjiro@gmail.com <taura.kenjiro@gmail.com@dd21a00a-c728-a333-246e-3318298a4437>
2014-10-09 15:36:54 +0000

    add cilkplus_dr.h for cilkplus and cilk_dr.cilkh for cilk

--- trunk/tpswitch/cilkplus_dr.h
+++ trunk/tpswitch/cilkplus_dr.h
@@ -4,10 +4,7 @@
 
 #pragma once
 
-#if __CILK__			/* MIT Cilk */
-#else				/* Cilk Plus */
 #include <cilk/cilk.h>
-#endif
 
 #if DAG_RECORDER>=2
 #include <dag_recorder.h>
@@ -16,13 +13,8 @@
 #define CILK_UNIFY_KEYWORDS 1
 
 #if CILK_UNIFY_KEYWORDS
-#if __CILK__
-#define cilk_spawn spawn 
-#define cilk_sync  sync
-#else  /* Cilk Plus */
 #define spawn cilk_spawn
 #define sync cilk_sync
-#endif
 #endif
 
 /* cilk interface; each cilk procedure (a procedure
@@ -30,9 +22,9 @@
 
 cilk int f(int x) {
   cilk_begin;
-  spawn_(spawn f(x));
-  spawn_(y = spawn g(x));
-  sync_;
+  spawn_(cilk_spawn f(x));
+  spawn_(y = cilk_spawn g(x));
+  cilk_sync_;
   cilk_return(y);
 }
 
@@ -40,12 +32,11 @@ cilk int f(int x) {
    (syntactically, it is a variable declaration,
    so you can put other variable decls after it)
 
-  - spawn f(x); -> spawn_(spawn f(x));
-  - sync        -> sync_;
+  - spawn f(x); -> spawn_(cilk_spawn f(x));
+  - sync        -> cilk_sync_;
 
   - return x;   -> cilk_return(x)
   - return;     -> cilk_void_return;
-
 
  */
 
@@ -63,20 +54,11 @@ cilk int f(int x) {
   } while(0)
 
 
-/* absorb diff between MIT Cilk and Cilkplus */
-#if __CILK__			/* MIT Cilk */
-#define cilk_or_cilkplus_spawn  spawn
-#define cilk_or_cilkplus_sync   sync
-#else                           /* GCC/ICC Cilkplus */
-#define cilk_or_cilkplus_spawn  cilk_spawn
-#define cilk_or_cilkplus_sync   cilk_sync
-#endif
-
 #define spawn_no_prof(spawn_stmt) spawn_stmt
 
 #define sync_no_prof do {				       \
     (void)__cilk_begin__;				       \
-    cilk_or_cilkplus_sync;				       \
+    cilk_sync;						       \
   } while(0)
 
 
@@ -106,7 +88,7 @@ cilk int f(int x) {
 #define sync_with_prof do {				       \
     dr_dag_node * __t__ = dr_enter_wait_tasks();	       \
     (void)__cilk_begin__;				       \
-    cilk_or_cilkplus_sync;				       \
+    cilk_sync;						       \
     dr_return_from_wait_tasks(__t__);			       \
   } while(0)
 
@@ -117,22 +99,12 @@ cilk int f(int x) {
 #define cilk_return(x)        cilk_return_with_prof(x)
 #define cilk_void_return      cilk_void_return_with_prof
 #define spawn_(spawn_stmt)    spawn_with_prof(spawn_stmt)
-#if CILK_UNIFY_KEYWORDS
-#define sync_                 sync_with_prof
 #define cilk_sync_            sync_with_prof
-#elif __CILK__
+#define _Cilk_sync_           sync_with_prof
 #define sync_                 sync_with_prof
-#else
-#define cilk_sync_            sync_with_prof
-#endif
 
-#if __CILK__ 			/* MIT Cilk */
-#define dr_get_max_workers()     Cilk_active_size
-#define dr_get_worker()          Self
-#else
 #define dr_get_max_workers()     __cilkrts_get_nworkers()
 #define dr_get_worker()          __cilkrts_get_worker_number()
-#endif
 
 #else
 
@@ -140,12 +112,8 @@ cilk int f(int x) {
 #define cilk_return(x)        cilk_return_no_prof(x)
 #define cilk_void_return      cilk_void_return_no_prof
 #define spawn_(spawn_stmt)    spawn_no_prof(spawn_stmt)
-#if CILK_UNIFY_KEYWORDS
-#define sync_                 sync_no_prof
 #define cilk_sync_            sync_no_prof
-#elif __CILK__
+#define _Cilk_sync_           sync_no_prof
 #define sync_                 sync_no_prof
-#else
-#define cilk_sync_            sync_no_prof
-#endif
+
 #endif
