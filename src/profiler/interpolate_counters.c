8b79c73324146ce6dc9f30c134d37fa586ebca5b
zanton <zanton.zzz@gmail.com>
2016-10-20 16:46:53 +0900

    fixed to find next *non-identical* sample used for interpolation

--- src/profiler/interpolate_counters.c
+++ src/profiler/interpolate_counters.c
@@ -11,14 +11,14 @@
    the value x0 at t0 and the value x1 at t1.
 
    t is typically but need not be between t0 and t1. */
-static unsigned long long
-interpolate_sample(unsigned long long t0, unsigned long long x0,
-		   unsigned long long t1, unsigned long long x1,
+static long long
+interpolate_sample(unsigned long long t0, long long x0,
+		   unsigned long long t1, long long x1,
 		   unsigned long long t) {
   (void)dr_check(t1 > t0);
   double a = (double)(t - t0) / (double)(t1 - t0);
   double b = (double)(t1 - t) / (double)(t1 - t0);
-  return (unsigned long long)(a * x1 + b * x0);
+  return (long long)(a * x1 + b * x0);
 }
 
 /* interpolate all counters of execution point q 
@@ -27,6 +27,7 @@ interpolate_sample(unsigned long long t0, unsigned long long x0,
 static void 
 interpolate_point(dr_clock_pos * q0, dr_clock_pos * q1, 
 		  dr_clock_pos * q, int nc) {
+  dr_check(q0->t < q1->t);
   int c;
   dr_clock_t t0 = q0->t;
   dr_clock_t t1 = q1->t;
@@ -34,9 +35,15 @@ interpolate_point(dr_clock_pos * q0, dr_clock_pos * q1,
   for (c = 0; c < nc; c++) {
     if (!q->counters[c]) {
       /* q's value is missing. interpolate it */
-      unsigned long long x0 = q0->counters[c];
-      unsigned long long x1 = q1->counters[c];
-      (void)dr_check((x0 && x1) || (!x0 && !x1));
+      long long x0 = q0->counters[c];
+      long long x1 = q1->counters[c];
+      //(void)dr_check(x0 <= x1);
+      if (x0 > x1) {
+        fprintf(stderr, "Warning: counter %d's value decreases from %lld (at t=%llu) to %lld (at t=%llu).\n",
+                c,
+                q0->counters[c], q0->t,
+                q1->counters[c], q1->t);
+      }
       q->counters[c] = interpolate_sample(t0, x0, t1, x1, t);
     }
   }
@@ -74,18 +81,29 @@ copy_point(dr_clock_pos * q0, dr_clock_pos * q, int nc) {
 */
 
 static dr_clock_pos ** 
-next_sample_of_worker(dr_clock_pos ** B, dr_clock_pos ** E, int w, int nc) {
+next_sample_of_worker(dr_clock_pos ** B, dr_clock_pos ** E, int w, int nc, dr_clock_t t0) {
   dr_clock_pos ** p;
   for (p = B; p < E && (*p)->worker == w; p++) {
     dr_clock_pos * q = *p;
-    int c;
-    for (c = 0; c < nc; c++) {
-      if (q->counters[c]) return p;
+    if (q->t > t0) {
+      int c;
+      for (c = 0; c < nc; c++) {
+        if (q->counters[c]) return p;
+      }
     }
   }
   return 0;
 }
 
+
+static void
+get_counter_value_string(char * s, dr_clock_pos * p, int nc) {
+  sprintf(s, "%lld", p->counters[0]);
+  int c;
+  for (c = 1; c < nc; c++) {
+    sprintf(s, "%s/%lld", s, p->counters[c]);
+  }
+}
 
 /*
    as the result of executing this function,
@@ -109,34 +127,67 @@ static dr_clock_pos **
 interpolate_worker(dr_clock_pos ** B, dr_clock_pos ** E, int w, int nc) {
   /* p0 is the next sample in B .. E. 
      0 if none is found */
-  dr_clock_pos ** p0 = next_sample_of_worker(B, E, w, nc);
+  dr_clock_pos ** p0 = next_sample_of_worker(B, E, w, nc, 0);
   /* p0 is the next sample in B .. E.
      0 if p0 is the only sample */
   dr_clock_pos ** p1 
-    = (p0 ? next_sample_of_worker(p0 + 1, E, w, nc) : 0);
+    = (p0 ? next_sample_of_worker(p0 + 1, E, w, nc, (**p0).t) : 0);
   dr_clock_pos ** p;
+  dr_check(!p0 || !p1 || (p1 != p0 && (*p1)->t > (*p0)->t));
 
   /* scan B */
   for (p = B; p < E && (*p)->worker == w; p++) {
+#if 0
+    dr_clock_pos temp_p = **p;
+    int need_interpolation = 0;
+    int c;
+    for (c = 0; c < nc; c++) {
+      if (temp_p.counters[c] == 0) {
+        need_interpolation = 1;
+        break;
+      }
+    }
+    char counter_value_str[100];
+    if (need_interpolation) {
+      get_counter_value_string(counter_value_str, &temp_p, nc);
+      fprintf(stderr, "sample (worker=%d,time=%llu,counters=%s) needs interpolation.. ",
+              temp_p.worker,
+              temp_p.t,
+              counter_value_str);
+    }
+#endif
     if (p1) {
       /* we have two samples.
-	 use two samples to interpolate it */
+         use two samples to interpolate it */
       interpolate_point(*p0, *p1, *p, nc);
+#if 0      
+      if (need_interpolation) {
+        get_counter_value_string(counter_value_str, *p, nc);
+        fprintf(stderr, "interpolated with p1, counters=%s\n", counter_value_str);
+      }
+#endif        
       if (p == p1) {
-	/* we reached the greater sample.
-	   try to find the next sample. */
-	dr_clock_pos ** p2 = next_sample_of_worker(p1, E, w, nc);
-	if (p2) {
-	  /* found. now shift the interval use for
-	     interpolation */
-	  p0 = p1;
-	  p1 = p2;
-	}
-	/* else (not found). i.e., p1 is the last sample.
-	   keep using (p0,p1) (extrapolation) */
+        /* we reached the greater sample.
+           try to find the next sample. */
+        dr_clock_pos ** p2 = next_sample_of_worker(p1 + 1, E, w, nc, (**p1).t);
+        dr_check(p2 != p1);
+        if (p2) {
+          /* found. now shift the interval use for
+             interpolation */
+          p0 = p1;
+          p1 = p2;
+        }
+        /* else (not found). i.e., p1 is the last sample.
+           keep using (p0,p1) (extrapolation) */
       }
     } else if (p0) {
       copy_point(*p0, *p, nc);
+#if 0
+      if (need_interpolation) {
+        get_counter_value_string(counter_value_str, *p, nc);
+        fprintf(stderr, "interpolated with p0, counters=%s\n", counter_value_str);
+      }
+#endif
     }
     /* else, do nothing. counters are left as zeros */
   }
@@ -168,7 +219,7 @@ dr_clock_pos_cmp(const void * a_, const void * b_) {
     return -1;
   if (a->t > b->t)
     return 1;
-  (void)dr_check(0);
+  //(void)dr_check(a_ == b_);
   return 0;
 }
 
